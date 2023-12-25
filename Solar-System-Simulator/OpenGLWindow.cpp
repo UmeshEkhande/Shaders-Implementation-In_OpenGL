ddef1293c19727b4355badf45f9b9eb73523c82e
UmeshEkhande <umesh.ekhande@cctech.co.in>
2023-12-25 12:51:50 +0530

    adding shader implementation project

---
+++ Solar-System-Simulator/OpenGLWindow.cpp
@@ -0,0 +1,234 @@
+#include "stdafx.h"
+#include "OpenGLWindow.h"
+#include <QOpenGLContext>
+#include <QOpenGLPaintDevice>
+#include <QOpenGLShaderProgram>
+#include <QPainter>
+#include <iostream>
+#include "SolarSystem.h"
+#include <QTimer>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <QFile>
+#include <QTextStream>
+
+OpenGLWindow::OpenGLWindow (const QColor& background, QWidget* parent) : mBackground(background)
+{
+	setParent(parent);
+	setMinimumSize(500, 250);
+	mZoomFactor = 1.25;
+    solar = new SolarSystem();
+	timer = new QTimer(this);
+	
+	const QStringList list = { "vShader.glsl","fShader.glsl"};
+	mShaderWatcher = new QFileSystemWatcher(list, this);
+	connect(mShaderWatcher, &QFileSystemWatcher::fileChanged, this, &OpenGLWindow::shaderWatcher);
+	 
+}
+
+void OpenGLWindow::updateData(std::vector<float>& vertices, std::vector<float>& colors)
+{
+	mVertices.clear();
+	mColors.clear();
+	mVertices = vertices;
+	mColors = colors;
+	update();
+}
+
+void OpenGLWindow::startRendering()
+{
+	connect(timer, &QTimer::timeout, this, &OpenGLWindow::updateSolarSystemData);
+	timer->start(16); 
+}
+
+void OpenGLWindow::stopRendering()
+{
+
+	timer->stop();
+}
+
+
+OpenGLWindow::~OpenGLWindow()
+{
+	reset();
+}
+
+void OpenGLWindow::reset()
+{
+	makeCurrent();
+	delete mProgram;
+	mProgram = nullptr;
+	doneCurrent();
+	QObject::disconnect(mContextWatchConnection);
+}
+
+
+
+void OpenGLWindow::updateSolarSystemData()
+{
+	mVertices.clear();
+	mColors.clear();
+	solar->drawSolarSystem(mVertices, mColors);
+	update();
+}
+
+void OpenGLWindow::setZoomFactor(GLfloat &zoomFactor)
+{
+	mZoomFactor = zoomFactor;
+	update();
+}
+
+void OpenGLWindow::setSpeedMultiplier(double speedMultiplier)
+{
+	solar->setSpeedMultiplier(speedMultiplier);
+}
+
+void OpenGLWindow::startRevolving()
+{	
+	timer->start(16);
+}
+
+void OpenGLWindow::stopRevolving()
+{
+	timer->stop();
+}
+
+void OpenGLWindow::resetPositions()
+{
+	
+	solar->resetPositions();
+	updateSolarSystemData();
+}
+
+void OpenGLWindow::updatePositions(int numberOfDays)
+{
+	solar->updatePositions(numberOfDays);
+	updateSolarSystemData();
+}
+
+void OpenGLWindow::mouseMoveEvent(QMouseEvent* event) {
+	int dx = event->x() - lastPos.x();
+	int dy = event->y() - lastPos.y();
+
+	if (event->buttons() & Qt::LeftButton) {
+		QQuaternion rotX = QQuaternion::fromAxisAndAngle(0.0f, 1.0f, 0.0f, 0.5f * dx);
+		QQuaternion rotY = QQuaternion::fromAxisAndAngle(1.0f, 0.0f, 0.0f, 0.5f * dy);
+
+		rotationAngle = rotX * rotY * rotationAngle;
+		update();
+	}
+	lastPos = event->pos();
+
+}
+
+
+
+void OpenGLWindow::paintGL()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	mProgram->bind();
+
+	QMatrix4x4 matrix;
+	//matrix.ortho(-6.0f * mZoomFactor, 6.0f * mZoomFactor, -6.0f * mZoomFactor, 6.0f * mZoomFactor, 0.1f, 100.0f);  // orthographic projection
+    matrix.ortho(-30.0f, 30.0f, -30.0f, 30.0f, 0.1f, 10.0f);
+	matrix.translate(0, 0, -2);
+	matrix.rotate(rotationAngle);
+
+	mProgram->setUniformValue(m_matrixUniform, matrix);
+	mProgram->setUniformValue(m_zoomUniform, mZoomFactor);
+
+	mVerticesData = mVertices.data();
+	mColorsData = mColors.data();
+
+	int size = mVertices.size();
+
+	glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, mVerticesData);
+	glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, mColorsData);
+
+	glEnableVertexAttribArray(m_posAttr);
+	glEnableVertexAttribArray(m_colAttr);
+
+	glDrawArrays(GL_LINES, 0, size / 2);
+
+	glDisableVertexAttribArray(m_colAttr);
+	glDisableVertexAttribArray(m_posAttr);
+
+}
+
+void OpenGLWindow::shaderWatcher()
+{
+	QString vertexShaderSource = readShaderSource("vShader.glsl");
+	QString fragmentShaderSource = readShaderSource("fShader.glsl");
+
+	mProgram->release();
+	mProgram->removeAllShaders();
+	
+	mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+	mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+
+	mProgram->link();
+}
+
+
+
+void OpenGLWindow::initializeGL()
+{
+
+	QString vertexShaderSource = readShaderSource("vShader.glsl");
+	QString fragmentShaderSource = readShaderSource("fShader.glsl");
+
+	initializeOpenGLFunctions();
+
+	mProgram = new QOpenGLShaderProgram(this);
+	mProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
+	mProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
+
+	if (!mProgram->link())
+	{
+		qDebug() << "Shader program link error:" << mProgram->log();
+
+	}
+
+	m_posAttr = mProgram->attributeLocation("posAttr");
+	m_colAttr = mProgram->attributeLocation("colAttr");
+	m_matrixUniform = mProgram->uniformLocation("matrix");
+	m_zoomUniform = mProgram->uniformLocation("zoom");
+
+
+
+	m_rotationAngleUniform = mProgram->uniformLocation("rotationAngle");  // Add this line
+
+
+	if (m_posAttr == -1 || m_colAttr == -1 || m_matrixUniform == -1)
+	{
+		qDebug() << "Shader attribute or uniform location error.";
+
+	}
+
+	glClearColor(mBackground.redF(), mBackground.greenF(), mBackground.blueF(), 1.0f);
+
+}
+
+QString OpenGLWindow::readShaderSource(QString filePath)
+{
+
+	QFile* file = new QFile(filePath);
+
+	if (!file->open(QFile::ReadOnly | QFile::Text))
+	{
+		std::cerr << "Invalid File !";
+		return "Invalid File !";
+	}
+
+	QTextStream stream(file);
+	QString fileString = stream.readLine();
+
+	while (!stream.atEnd())
+	{
+		fileString.append(stream.readLine());
+	}
+	return fileString;
+
+}
+
---
+++ Solar-System-Simulator/OpenGLWindow.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <cmath>
+#include <QTimer>
+#include "SolarSystem.h"
+
+class QOpenGLTexture;
+class QOpenGLShader;
+class QOpenGLShaderProgram;
+class QOpenGLPaintDevice;
+class SolarSystem;
+
+class OpenGLWindow :public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+public:
+    OpenGLWindow(const QColor& background, QWidget* parent);
+    ~OpenGLWindow();
+
+    void setZoomFactor(GLfloat &zoomFactor);
+    void setSpeedMultiplier(double speedMultiplier);
+    void startRevolving();
+    void stopRevolving();
+    void resetPositions();
+    void updatePositions(int numberOfDays);
+    void updateSolarSystemData();
+    void startRendering();
+    void stopRendering();
+    void updateData(std::vector<float>& vertices, std::vector<float>& colors);
+
+protected:
+    void paintGL() override;
+    void initializeGL() override;
+
+    
+private:
+    void reset();
+    QString readShaderSource(QString filePath);
+    void mouseMoveEvent(QMouseEvent* event);
+    void shaderWatcher();
+
+
+private:
+    QTimer* timer;
+    SolarSystem* solar;
+    QOpenGLShaderProgram* mProgram = nullptr;
+    QColor mBackground;
+    QMetaObject::Connection mContextWatchConnection;
+    GLint m_posAttr = 0;
+    GLint m_colAttr = 0;
+    GLint m_matrixUniform = 0;
+    GLint m_zoomUniform;
+    GLint m_rotationAngleUniform;  // Add this line
+
+
+    float* mVerticesData;
+    float* mColorsData;
+    GLfloat mZoomFactor;
+    bool mIsRevolving;
+
+    std::vector<float> mVertices;
+    std::vector<float> mColors;
+
+    QQuaternion rotationAngle;
+    QPoint lastPos;
+
+    QFileSystemWatcher* mShaderWatcher;
+};
